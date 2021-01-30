d106d7ecdf164e1bbe0a8a521c10f048060f3659
Toby Hogan <tobyhogan@macusers-iMac.home>
2021-01-30 13:02:25 +0000

    worked on security

--- Data/security.py
+++ Data/security.py
@@ -2,8 +2,13 @@ import bcrypt
 
 
 def hash(password):
-    return bcrypt.hashpw(password, gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def check_hash(password, hash):
-    return True if bcrypt.checkpw(password, hash) else False
+    return True if bcrypt.checkpw(password.encode('utf-8'), hash) else False
+
+
+
+
+
