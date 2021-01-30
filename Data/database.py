adf2a8c10d6b4f3de31b4fc75a719864686aa43d
Toby Hogan <tobyhogan@macusers-iMac.home>
2021-01-30 13:13:28 +0000

    added database features

--- Data/database.py
+++ Data/database.py
@@ -1,13 +1,30 @@
 import sqlite3
-import Data.security
+import Data.security as security
 
 
 def add_user(username, password, theme, volume):
+    password = security.hash(password)
+    print(password)
+
     conn = sqlite3.connect('Data/user_info.db')
     c = conn.cursor()
 
     c.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (username, password, theme, volume))
 
+    conn.commit()
+    conn.close()
+
+
+def check_user(username, password):
+    conn = sqlite3.connect('Data/user_info.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM users WHERE username = ?", (username,))
+    return True if security.check_hash(password, c.fetchone()[1]) else False
 
     conn.commit()
     conn.close()
+
+
+def update_user():
+    pass
