53695d6c4609542325d45a38b40c16e2b4b6d899
Toby Hogan <tobyhogan@macusers-iMac.home>
2021-01-30 12:36:00 +0000

    started working on databasing

--- Data/database.py
+++ Data/database.py
@@ -1,3 +1,15 @@
 import sqlite3
 
 conn = sqlite3.connect('high_scores.db')
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE customers (
+        username text,
+        password text,
+        
+    )""")
+
+cursor.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@codemy.com')")
+
+conn.commit()
+conn.close()
