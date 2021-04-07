78d6bc4526bf0c10001558bdcc39d96abe865f72
Toby Hogan <tobyhogan44@gmail.com>
2021-04-07 16:20:31 +0100

    commented security

--- data/security.py
+++ data/security.py
@@ -2,42 +2,35 @@ import bcrypt
 import re
 
 
+# imports the regex and bcrypt modules - regex used to make advanced patterns for searching, bcrypt for encryption
+
 def hash(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+# makes a hash from a password, also salts the password before hashing for added security
 
 
 def check_hash(password, hash):
     return True if bcrypt.checkpw(password.encode('utf-8'), hash) else False
 
 
-def password_check_hard(password):
-    """
-    Verify the strength of 'password'
-    Returns a dict indicating the wrong criteria
-    A password is considered strong if:
-        8 characters length or more
-        1 digit or more
-        1 symbol or more
-        1 uppercase letter or more
-        1 lowercase letter or more.
-    """
+# compares a password to hashed and salted password to see if they are the same - used for user login
 
-    # calculating the length
+
+def password_check_hard(password):
     length_error = len(password) < 8
 
-    # searching for digits
     digit_error = re.search(r"\d", password) is None
 
-    # searching for uppercase
     uppercase_error = re.search(r"[A-Z]", password) is None
 
-    # searching for lowercase
     lowercase_error = re.search(r"[a-z]", password) is None
 
-    # searching for symbols
     symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', password) is None
 
-    # overall result
     password_ok = not (length_error or digit_error or uppercase_error or lowercase_error or symbol_error)
 
     return password_ok
+
+# function to check a password meets strength criteria
